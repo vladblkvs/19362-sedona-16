2a0a74242783f8177aaf47bdda5ac4e4afcef169
vladblkvs <gremlinux@gmail.com>
2019-05-02 02:42:48 +0300

    Исправляет сжатие html файла

--- gulpfile.js
+++ gulpfile.js
@@ -60,7 +60,7 @@ gulp.task("html", function() {
     .pipe(posthtml([
       include()
     ]))
-    .pipe(htmlmin())
+    .pipe(htmlmin({ collapseWhitespace: true }))
     .pipe(gulp.dest("build"))
 })
 
