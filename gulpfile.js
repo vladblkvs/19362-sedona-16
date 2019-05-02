7d1f606d6a0d8a514adca3d8ec58730347c9d057
vladblkvs <gremlinux@gmail.com>
2019-05-02 14:54:37 +0300

    Оптимизирует шрифты и файлы скриптов
    Шрифты подгружаются через preload, добавлен стиль display: swap
    Скрипты проходят минификацию
    Svg спрайт собирается из специальной папки с исходниками, которая не
    копируется в билд

--- gulpfile.js
+++ gulpfile.js
@@ -8,6 +8,7 @@ var cssnano = require("gulp-cssnano");
 var posthtml = require("gulp-posthtml");
 var include = require("posthtml-include");
 var htmlmin = require("gulp-htmlmin");
+var uglify = require("gulp-uglify");
 var imagemin = require("gulp-imagemin");
 var webp = require("gulp-webp");
 var svgstore = require("gulp-svgstore");
@@ -47,7 +48,7 @@ gulp.task("webp", function() {
 });
 
 gulp.task("sprite", function() {
-  return gulp.src("source/img/icon-*.svg", "source/img/logo-htmlacademy.svg")
+  return gulp.src("source/img/**/*.svg")
     .pipe(svgstore({
       inlineSvg: true
     }))
@@ -62,14 +63,26 @@ gulp.task("html", function() {
     ]))
     .pipe(htmlmin({ collapseWhitespace: true }))
     .pipe(gulp.dest("build"))
-})
+});
+
+gulp.task("js", function() {
+  return gulp.src("source/js/*.js")
+    .pipe(uglify())
+    .pipe(rename(function(path) {
+      path.basename += ".min";
+      path.extname = ".js";
+    }))
+    .pipe(gulp.dest("build/js"))
+});
 
 gulp.task("copy", function() {
   return gulp.src([
       "source/fonts/**/*.{woff,woff2}",
       "source/img/**",
-      "source/js/**",
-      "source/*.{ico,png}"
+      "source/*.{ico,png}",
+      "!source/img/sprite/**",
+      "!source/img/bg-triangle.svg",
+      "!source/img/bg-intro-triangle-*.svg"
     ], {
       base: "source"
     })
@@ -99,5 +112,5 @@ gulp.task("refresh", function(done) {
   done();
 });
 
-gulp.task("build", gulp.series("clean", "copy", "css", "sprite", "html"));
+gulp.task("build", gulp.series("clean", "copy", "css", "sprite", "html", "js"));
 gulp.task("start", gulp.series("build", "server"));
